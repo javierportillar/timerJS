28da1f320c667035522d629f13fbeca0a37f5831
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2023-12-12 18:56:52 -0500

    fixing timer Bug, I had to evaluate the status of the variable time, because if start is clicked over once, it was creating multiple intervales at the same time, this makes that decrementTime() was called over once per second

--- index.js
+++ index.js
@@ -1,129 +1,3 @@
-/* Timer with only one input
-Declaración de variables para los botones
-const start = document.querySelector("#start");
-const stop = document.querySelector("#stop");
-const reset = document.querySelector("#reset");
-
-// Declaración de variables para el temporizador
-const hours = document.querySelector("#hours");
-const minutes = document.querySelector("#minutes");
-const seconds = document.querySelector("#seconds");
-const currentTime = document.getElementById("current-time");
-
-setInitialValues();
-
-start.addEventListener("click", startTimer);
-stop.addEventListener("click", stopTimer);
-reset.addEventListener("click", resetTimer);
-
-// Variable para mantener el intervalo
-var time = null;
-
-// Función para iniciar el temporizador
-function startTimer() {
-  buttonsAction();
-  // Convertir valores a números y manejar campos vacíos
-  let h = parseInt(hours.value) || 0;
-  let m = parseInt(minutes.value) || 0;
-  let s = parseInt(seconds.value) || 0;
-
-  if (h < 0 || m < 0 || s < 0 || m > 59 || s > 59) {
-    alert("Please enter a valid time");
-    return;
-  }
-
-  if (h === 0 && m === 0 && s === 0) {
-    alert("Please enter a valid time");
-    return;
-  }
-
-  if (!time) {
-    time = setInterval(decrementTime, 1000);
-  }
-}
-
-function decrementTime() {
-  let h = parseInt(hours.value) || 0;
-  let m = parseInt(minutes.value) || 0;
-  let s = parseInt(seconds.value) || 0;
-
-  // Lógica para decrementar el tiempo
-  if (s > 0) {
-    s--;
-  } else {
-    if (m > 0) {
-      s = 59;
-      m--;
-    } else if (h > 0) {
-      m = 59;
-      s = 59;
-      h--;
-    } else {
-      stopTimer();
-      setInitialValues();
-      alert("Timer finished!");
-      return;
-    }
-  }
-  // Actualizar los campos de texto
-  actData(h, m, s);
-}
-
-// Función para detener el temporizador
-function stopTimer() {
-  clearInterval(time);
-  time = null;
-}
-
-// Función para resetear el temporizador
-function resetTimer() {
-  setInitialValues();
-  currentTime.innerHTML = `Current time:`; // Reinicia el texto
-  stopTimer(); // Detiene el temporizador
-  hours.value = 0;
-  minutes.value = 0;
-  seconds.value = 0;
-}
-
-function buttonsAction() {
-  seconds.disabled = true;
-  minutes.disabled = true;
-  hours.disabled = true;
-  start.disabled = true;
-  stop.disabled = false;
-  reset.disabled = false;
-}
-
-function setInitialValues() {
-  seconds.disabled = false;
-  minutes.disabled = false;
-  hours.disabled = false;
-  start.disabled = false;
-  stop.disabled = true;
-  reset.disabled = true;
-}
-
-function actData(h, m, s) {
-  hours.value = h;
-  minutes.value = m;
-  seconds.value = s;
-  h = h.toString();
-  m = m.toString();
-  s = s.toString();
-
-  if (h.length < 2) {
-    h = "0" + h;
-  }
-  if (m.length < 2) {
-    m = "0" + m;
-  }
-  if (s.length < 2) {
-    s = "0" + s;
-  }
-  currentTime.innerHTML = `Current time: ${h}h:${m}m:${s}s`;
-}
-*/
-
 //Multiple Timers
 class Timer {
   constructor(
@@ -216,13 +90,17 @@ class Timer {
       return;
     }
 
-    if (!this.time) {
-      this.time = setInterval(() => this.decrementTime(), 1000);
+    if (this.time) {
+      clearInterval(this.time);
     }
+    this.time = setInterval(() => this.decrementTime(), 1000);
   }
 
   stop() {
-    this.time = null;
+    if (this.time) {
+      clearInterval(this.time);
+      this.time = null;
+    }
     this.enableInputs();
   }
 
